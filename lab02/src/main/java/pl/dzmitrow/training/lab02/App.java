dc4f08aaebcabc87b842f269b976e8168ae186c6
Dominik Zmitrowicz <dogrizz@gmail.com>
2016-04-20 15:46:28 +0200

    working publish-subscribe

--- lab02/src/main/java/pl/dzmitrow/training/lab02/App.java
+++ lab02/src/main/java/pl/dzmitrow/training/lab02/App.java
@@ -1,4 +1,15 @@
 package pl.dzmitrow.training.lab02;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
+
+    public static void main(String[] args){
+        try(ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml")){
+            while(true){
+
+            }
+        }
+    }
+
 }
