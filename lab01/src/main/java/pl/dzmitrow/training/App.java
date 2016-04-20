ffd1f1d874d948ec85dbb0bc202d1be3f54c3870
Dominik Zmitrowicz <dogrizz@gmail.com>
2016-04-20 15:21:37 +0200

    DateTime gateway

--- lab01/src/main/java/pl/dzmitrow/training/App.java
+++ lab01/src/main/java/pl/dzmitrow/training/App.java
@@ -5,12 +5,18 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
+import java.util.Date;
+
 public class App {
     public static void main(String[] args) {
         try(ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml")){
             MessageChannel channel = applicationContext.getBean("messages",MessageChannel.class);
             Message<String> message = MessageBuilder.withPayload("dd-MM-yyyy hh:mm:ss").build();
             channel.send(message);
+
+            DateTime dateTime = applicationContext.getBean("dateTimeGateway", DateTime.class);
+            dateTime.print("dd-MM-yyyy");
+            System.out.println(dateTime.get("dd-MM-yyyy"));
         }
     }
 }
