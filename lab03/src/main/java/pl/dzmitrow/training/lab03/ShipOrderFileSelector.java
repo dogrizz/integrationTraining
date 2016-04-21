1a1ed5c7b8817cec847ec92e4c9aa8c6d023e2b5
Dominik Zmitrowicz <dogrizz@gmail.com>
2016-04-21 09:56:30 +0200

    Working file name selector on file channel

---
+++ lab03/src/main/java/pl/dzmitrow/training/lab03/ShipOrderFileSelector.java
@@ -0,0 +1,21 @@
+package pl.dzmitrow.training.lab03;
+
+import org.springframework.integration.core.MessageSelector;
+import org.springframework.messaging.Message;
+
+import java.io.File;
+
+public class ShipOrderFileSelector implements MessageSelector {
+
+    private static final String PREFIX = "shiporder";
+
+    @Override
+    public boolean accept(Message<?> message) {
+        Object payload = message.getPayload();
+        if(payload instanceof File){
+            File file = (File) payload;
+            return file.getName().startsWith(PREFIX);
+        }
+        return false;
+    }
+}
